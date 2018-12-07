b61435835e0fba74f4a2f0c7b4cb7b7c01514e99
vbommena <vishaal.bommena@gmail.com>
2018-12-07 12:54:53 -0500

    new changes

--- Backend/app.py
+++ Backend/app.py
@@ -11,6 +11,11 @@ def removeHW():
         conn_string = "host='ec2-54-197-234-33.compute-1.amazonaws.com' dbname='db1spps0dlvmqh' port='5432' user='esijiopzwrpisz' password='0e12254844a007713161373b426528ef20a87be02da9da4cb641d1242767133d'"
         con = psycopg2.connect(conn_string)
         curs = con.cursor()
+        text = ast.literal_eval(json.dumps(request.get_json()))
+        user = text['user']
+        hwName = text['hwName']
+        collegeN = (user, hwName, )
+        curs.execute("DELETE FROM %s WHERE hwName = %s", collegeN)
         con.commit()
         curs.close()
     finally:
