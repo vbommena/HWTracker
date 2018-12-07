9849654ffa32ade0c925da94c9c95ac0cc9425cc
vbommena <vishaal.bommena@gmail.com>
2018-12-07 13:53:08 -0500

    new changes

--- Backend/app.py
+++ Backend/app.py
@@ -10,22 +10,17 @@ def removeHW():
         print ("Endpoint reached")
         conn_string = "host='ec2-174-129-41-12.compute-1.amazonaws.com' dbname='d5voje17q647tn' port='5432' user='atvungxynkajkc' password='42b9b99a8246ed64266e426afa1ddfe3288b5de68eab0f98bd96142b501e0a81'"
         con = psycopg2.connect(conn_string)
-        curs = con.cursor()
         hwName = request.form["hwName"]
         user = "vbommena1"
         collegeN = (user, hwName, )
         print (collegeN)
-        curs.execute("DELETE FROM %s WHERE hwName = %s", collegeN)
-        curs1 = con.cursor()
-        print (user)
         curs1.execute("SELECT * FROM %s", (user, ))
         result = []
         for row in curs1:
             result.append(row)
         print (result)
         con.commit()
-        curs.close()
-        curs1.close
+        curs1.close()
     finally:
         if con:
             con.close()
