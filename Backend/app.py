1a3037884e7221a5849233b9dec5df4896c82031
vbommena <vishaal.bommena@gmail.com>
2018-12-07 12:45:06 -0500

    new changes

--- Backend/app.py
+++ Backend/app.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 def removeHW():
     con = None
     try:
-        print "Endpoint reached"
+        print ("Endpoint reached")
         conn_string = "host='ec2-54-197-234-33.compute-1.amazonaws.com' dbname='db1spps0dlvmqh' port='5432' user='esijiopzwrpisz' password='0e12254844a007713161373b426528ef20a87be02da9da4cb641d1242767133d'"
         con = psycopg2.connect(conn_string)
         curs = con.cursor()
-
         con.commit()
         curs.close()
     finally:
